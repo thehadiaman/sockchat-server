31c5bca50a916705e0e493aee058391954908baa
HADI AMAN <hadiaman2.0@gmail.com>
2021-11-20 13:18:21 +0530

    Route to check the existence of an account using username or email.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const {userValidation} = require("../validation/users");
+const {validation} = require("../validation/users");
 const {User} = require("../database/users");
 const { sendEmail } = require("../validation/email");
 const router = require('express').Router();
 
 router.post('/', async(req, res)=>{
-    const {error} = userValidation(req.body);
+    const {error} = validation('userSchema', req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     const user = await User.getUser({email: req.body.email});
@@ -16,6 +16,22 @@ router.post('/', async(req, res)=>{
     res.send("User saved.");
 });
 
+router.get('/:id', async(req, res)=>{
+    const id = req.params.id;
+    const {error: usernameError} = validation('usernameSchema', {username: id});
+    const {error: emailError} = validation('emailSchema', {email: id});
 
+    if(usernameError && emailError){
+        return res.status(400).send('Invalid values.');
+    }else if(usernameError){
+        const user = await User.getUser({email: id});
+        if(user) return res.send(true);
+    }else if(emailError){
+        const user = await User.getUser({username: id});
+        if(user) return res.send(true);
+    }
+
+    res.send(false);
+});
 
 module.exports = router;
--- validation/users.js
+++ validation/users.js
@@ -1,12 +1,21 @@
 const Joi = require('joi');
 
-const userSchema = Joi.object({
-    email: Joi.string().min(8).max(50).email().required(),
-    username: Joi.string().alphanum().min(3).max(50).required(),
-    name: Joi.string().alphanum().min(3).max(50).required(),
-    password: Joi.string().min(6).max(50).required()
-});
-
-exports.userValidation = function(body){
-    return userSchema.validate(body);
+const schemas = {
+    userSchema: Joi.object({
+        email: Joi.string().min(8).max(50).email().required(),
+        name: Joi.string().min(3).max(50).required(),
+        username: Joi.string().alphanum().min(3).max(50).required(),
+        password: Joi.string().min(6).max(50).required()
+    }),
+    usernameSchema: Joi.object({
+        username: Joi.string().alphanum().min(3).max(50).required()
+    }),
+    emailSchema: Joi.object({
+        email: Joi.string().min(8).max(50).email().required()
+    })
 };
+
+exports.validation = function(schema, body){
+    return schemas[schema].validate(body);
+};
+
