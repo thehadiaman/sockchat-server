140f0fd093febdba8c79aa01ab0d55a37473a418
HADI AMAN <hadiaman2.0@gmail.com>
2021-12-09 10:59:41 +0530

    "socket.io" web stocket integrated with server side.

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const app = require('express')();
 const config = require('config');
 require('express-async-errors');
 
+const {createServer} = require('http');
+const server = createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+      }
+});
+
 // require('./startup/logging')();
 require('./startup/config')();
 require('./startup/cors')(app);
 require('./startup/database')();
 require('./startup/routes')(app);
 
-app.listen(config.get('PORT'), () => {
-    console.log(`Listening in port ${config.get('PORT')}`);
+io.on('connection', (socket) => {
+    console.log('New user is connected.');
+});
+
+server.listen(config.get('PORT'), ()=>{
+    console.log(`Listening on port ${config.get('PORT')}`);
 });
